8d7685dc7b336400c21a47500a9e5073e5841b81
Joe Kinney <joe@enterpagoda.com>
2021-04-23 17:38:39 -0400

    end day 1

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 // notes from lectures
+
+// channel for data to go in and out over the internet
+// open socket to internet
+// label is not automatically available to us in js (like settimeout); have to announce we want it
+// returns object with methods to the variable server
+const server = http.createServer();
+server.listen(80);
